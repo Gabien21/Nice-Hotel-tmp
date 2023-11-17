e90286c265a78328d0c080e59bc4604dac23c34b
Gabien21 <pktoan21@clc.fitus.edu.vn>
2023-11-17 17:47:31 +0700

    update get  staff list, get guest list, find one user by email function

--- server/routes/users.js
+++ server/routes/users.js
@@ -35,13 +35,27 @@ router.get("/info", verifyToken, async (req, res) => {
   }
 });
 
-// @route GET api/user/info
-// @desc Get user infomation
-// @access Private
-router.get("/find_info", verifyToken, async (req, res) => {
+// @route GET api/user/find_info/:email
+// @desc Get user infomation by email
+// @access Private (only use for staff, admin)
+router.get("/find_info/:email", verifyToken, async (req, res) => {
     try {
+        console.log(req.params.email)
       const user = await User.findOne({ _id: req.userId });
-      if (!user) {
+      const user_find = await User.findOne({ email: req.params.email });
+      if (user.role == 'Guest') {
+        return res.status(200).json({
+            success: false,
+            message: "Access denied"
+          });
+      }
+      if (user.role == 'Staff' && (user_find.role == 'Business_Admin' || user_find.role == 'System_Admin')) {
+        return res.status(200).json({
+            success: false,
+            message: "User not found"
+          });
+      }
+      if (!user_find) {
           return res.status(200).json({
             success: false,
             message: "User not found",
@@ -49,13 +63,95 @@ router.get("/find_info", verifyToken, async (req, res) => {
         }
       res.json({
         success: true,
+        email: user_find.email,
+        full_name: user_find.full_name,
+        address: user_find.address,
+        birthday: user_find.birthday,
+        user_id: user_find.user_id,
+        role: user_find.role,
+        createAt: user_find.createAt,
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        success: false,
+        message: "Internal server error",
+      });
+    }
+  });
+
+// @route GET api/user/find_info
+// @desc Get guest list
+// @access Private (only use for staff, admin)
+router.get("/get_guest_list", verifyToken, async (req, res) => {
+    try {
+      const user_find = await User.findOne({ _id: req.userId });
+      if(user_find.role == 'Guest') {
+        return res.status(200).json({
+            success: false,
+            message: "Access denied!"
+          });
+      }
+      const user = await User.find({ role: 'Guest' });
+      if (!user) {
+        return res.status(200).json({
+            success: false,
+            message: "No guest!"
+          });
+      }
+
+      const guestList = user.map(user => ({
         email: user.email,
-        full_name: user.full_name,
-        address: user.address,
+        full_name: user.full_name,  
         birthday: user.birthday,
-        user_id: user.user_id,
-        role: user.role,
-        createAt: user.createAt,
+        phone_number: user.phone_number,
+        address: user.address
+      }));
+
+      res.json({
+        success: true,
+        guestList: guestList
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({
+        success: false,
+        message: "Internal server error",
+      });
+    }
+  });
+
+// @route GET api/user/find_info
+// @desc Get staff list
+// @access Private (only use for admin)
+router.get("/get_staff_list", verifyToken, async (req, res) => {
+    try {
+      const user_find = await User.findOne({ _id: req.userId });
+      if(user_find.role == 'Guest' || user_find.role == 'Staff') {
+        return res.status(200).json({
+            success: false,
+            message: "Access denied!"
+          });
+      }
+      const user = await User.find({ role: 'Staff' });
+      if (!user) {
+        return res.status(200).json({
+            success: false,
+            message: "No guest!"
+          });
+      }
+
+      const guestList = user.map(user => ({
+        email: user.email,
+        full_name: user.full_name,  
+        birthday: user.birthday,
+        phone_number: user.phone_number,
+        address: user.address
+      }));
+
+      res.json({
+        success: true,
+        guestList: guestList
       });
     } catch (error) {
       console.log(error);
